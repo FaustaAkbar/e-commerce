48b50f2ac0e976c4c7e634806ab68ab3c7fc4dc8
Fausta Akbar Wijaya <faussss1357@gmail.com>
2024-10-04 22:31:47 +0700

    create Color Palete

---
+++ pet_shop/lib/widget/color_palete.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF6200EE);
+  static const Color secondaryColor = Color(0xFF03DAC6);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color accentColor = Color(0xFF018786);
+  static const Color textPrimary = Color(0xFF000000);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color errorColor = Color(0xFFB00020);
+  static const Color lightGray = Color(0xFFBDBDBD);
+  static const Color darkGray = Color(0xFF424242);
+}
