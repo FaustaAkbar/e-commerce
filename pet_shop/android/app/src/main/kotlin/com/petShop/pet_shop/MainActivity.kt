d831dff5bd4d00471b1bbbf78bed081da6a45f58
Fausta Akbar Wijaya <faussss1357@gmail.com>
2024-10-30 08:31:00 +0700

    initial zekOrder

--- pet_shop/android/app/src/main/kotlin/com/petShop/pet_shop/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.petShop.pet_shop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ zekOrder/script.js
@@ -0,0 +1,4 @@
+// Placeholder for future functionality like search button or profile actions
+document.querySelector('.search-btn').addEventListener('click', function() {
+    alert('Search functionality coming soon!');
+});
