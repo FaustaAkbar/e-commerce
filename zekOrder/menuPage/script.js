08a771600ddfb9d90aac39e5e48aecf508475ee6
Georgio Armando Woda Kolo <georgioarmando8@gmail.com>
2024-10-31 18:42:47 +0700

    HomeMenu-Page

--- zekOrder/menuPage/script.js
+++
@@ -1,4 +0,0 @@
-// Placeholder for future functionality like search button or profile actions
-document.querySelector('.search-btn').addEventListener('click', function() {
-    alert('Search functionality coming soon!');
-});
