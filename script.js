747ce8492368b266b513c254cbe273aa87ed589e
Georgio Armando Woda Kolo <georgioarmando8@gmail.com>
2024-10-31 16:12:21 +0700

    Remove all files

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleSearchBox() {
-  var searchBox = document.getElementById('search-box');
-  if (searchBox.style.display === 'none') {
-    searchBox.style.display = 'block';
-  } else {
-    searchBox.style.display = 'none';
-  }
-}
